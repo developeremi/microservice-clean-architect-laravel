733263b451503b126a68faf27d0c7284ee361328
unknown <mehrdad.masoumi517@gmail.com>
2024-05-11 19:00:48 +0330

    first commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,17 +3,26 @@
 namespace App\Http\Controllers;
 
 use Src\Service\OrderService;
+use Src\Service\UserService;
 
 class UserController extends Controller
 {
 
     public function __construct(
-      protected OrderService $service
+        protected UserService $userService,
+        protected OrderService $orderService
     ) {}
+
+
+    public function me()
+    {
+        return $this->userService->profile(1);
+    }
+
 
     public function userOrders($user_id)
     {
 
-        return $this->service->userOrderCount($user_id);
+        return $this->orderService->userOrderCount($user_id);
     }
 }
